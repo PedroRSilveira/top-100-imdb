d2e062bd715ffe97b79ff7ba5e8478350c469d6d
Pedro Rodrigues Silveira <pedro.rsilveira@goias.gov.br>
2024-05-02 11:51:41 -0300

    Atualizações 02-05

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,25 +12,79 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Arquivo.imprimirLista();
+		System.out.println("\t\t\tSeja bem vindo ao sistema de registros do top 100 filmes mais bem avaliados do IMDb\n"
+						  + "\t\t\t\tSeu objetivo é assistir 100 filmes, até agora você assistiu " + Arquivo.lerLista().size() + " filmes\n");
 		
-		System.out.println();
-		System.out.println("Teste de inserção:");
-		
-		System.out.print("Digite o nome do filme: ");
-		String nome = sc.nextLine();
-		System.out.print("Digite a posição no ranking do filme: ");
-		Integer pos = sc.nextInt();
-		
-		Filme filme = new Filme(nome, pos);
-		
-		Arquivo.adicionarFilme(filme);
-		
-		System.out.println();
-		
-		
-		Arquivo.imprimirLista();
-		
+		int opcao;
+		do {
+			System.out.println("O que quer fazer agora?"
+					  + "\n1. Ver lista de filmes assistidos"
+					  + "\n2. Adicionar filme a lista de assistidos"
+					  + "\n3. Editar lista de filmes assistidos"
+					  + "\n4. Sortear um filme para ser assistido"
+					  + "\n5. Encerrar programa\n");
+			
+			opcao = sc.nextInt();
+			System.out.println();
+			
+			switch(opcao) {
+			case 1:
+				Arquivo.imprimirLista();
+				System.out.println();
+				break;
+				
+			case 2:
+				sc.nextLine();
+				System.out.print("Digite o nome do filme: ");
+				String nome = sc.nextLine();
+				System.out.print("Digite a posição no ranking do filme: ");
+				Integer pos = sc.nextInt();
+				Filme filme = new Filme(nome, pos);
+				Arquivo.adicionarFilme(filme);
+				System.out.println();
+				break;
+				
+			case 3:
+				System.out.println("Qual filme (posição) quer editar?");
+				int posfilme = sc.nextInt();
+				if(posfilme < 0 || posfilme > 100) {
+					System.out.println("Valor inválido, escolha um número entre 1 e 100\n");
+				}else {
+					Arquivo.editarFilme(posfilme);
+					System.out.println("Você quer editar o nome/posição ou excluir um filme assistido?"
+									  + "\n1. Nome/Posição"
+									  + "\n2. Excluir"
+									  + "\n3. Cancelar");
+					int opc = sc.nextInt();
+					if(opc == 1) {
+						Arquivo.editarFilme(posfilme);
+					}else if(opc == 2) {
+						Arquivo.excluirFilme(posfilme);
+					}else {
+						System.out.println("Operação cancelada");
+					}
+					
+					System.out.println();
+				}
+				break;
+				
+			case 4:
+				Arquivo.sortear();
+				System.out.println();
+				break;
+			
+			case 5:
+				System.out.println("Encerrando programa");
+				break;
+				
+			default:
+				System.out.println("Opção inválida, escolha um número entre 1 e 5");	
+				System.out.println();
+			}
+			
+		}while(opcao != 5);
+
+		System.out.println("Programa finalizado");
 		sc.close();
 	}
 }
--- src/entities/Arquivo.java
+++ src/entities/Arquivo.java
@@ -9,17 +9,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class Arquivo {
 	
 	public Arquivo() {
 	}
 	
-	private static String path = "C:\\temp\\filmes.txt";
+	private static String path = "filmes.txt";
 	
-	private static List<Filme> lerLista() {
+	public static List<Filme> lerLista() {
 		List<Filme> list = new ArrayList<>();
-		String path = "C:\\temp\\filmes.txt";
+		String path = "filmes.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
@@ -69,4 +70,37 @@ public class Arquivo {
 		salvarLista(filmes);
 	}
 	
+	private static List<Integer> filmesAssistidos(List<Filme> filmes) {
+		List<Integer> assistidos = new ArrayList<Integer>();
+		for(Filme filme : filmes) {
+			int pos = filme.getPosicao();
+			assistidos.add(pos);
+		}
+		return assistidos;
+	}
+	
+	static List<Integer> assistidos = filmesAssistidos(lerLista());
+	
+	public static void sortear() {
+		if(assistidos.size() < 100) {
+			Random random = new Random();
+			int proximoFilme;
+			
+			do {
+				proximoFilme = random.nextInt(100) + 1;
+			}while(assistidos.contains(proximoFilme));
+			
+			System.out.println("O próximo filme a ser assistido é: " + proximoFilme);
+		}else {
+			System.out.println("Você já completou a lista de filmes!");
+		}
+	}
+	
+	public static void editarFilme(int pos) {
+		
+	}
+	
+	public static void excluirFilme(int pos) {
+		
+	}
 }
